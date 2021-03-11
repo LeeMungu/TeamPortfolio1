80325a87f72296a3028e0d07afb68461eb05a34e
whara123 <64121533+whara123@users.noreply.github.com>
2021-03-11 20:31:37 +0900

    잠시

--- Zelter23Day/01_WinMain/HousingObject.cpp
+++ Zelter23Day/01_WinMain/HousingObject.cpp
@@ -1,8 +1,34 @@
 #include "pch.h"
 #include "HousingObject.h"
+#include "Tile.h"
+#include <fstream>
 
-HousingObject::HousingObject(const string& name)
+HousingObject::HousingObject(const string& name, float x, float y, SideType type)
+	:GameObject(name)
 {
+	mX = x;
+	mY = y;
+
+	mTileList.assign(TileCountY, vector<Tile*>());
+	for (int y = 0; y < mTileList.size(); ++y)
+	{
+		for (int x = 0; x < TileCountX; ++x)
+		{
+			mTileList[y].push_back(new Tile
+			(
+				nullptr,
+				TileSize * x,
+				TileSize * y,
+				TileSize,
+				TileSize,
+				0,
+				0
+			));
+		}
+	}
+	sidetype = type;
+
+	Load();
 }
 
 void HousingObject::Init()
@@ -11,6 +37,13 @@ void HousingObject::Init()
 
 void HousingObject::Release()
 {
+	for (int y = 0; y < TileCountY; ++y)
+	{
+		for (int x = 0; x < TileCountX; ++x)
+		{
+			SafeDelete(mTileList[y][x]);
+		}
+	}
 }
 
 void HousingObject::Update()
@@ -19,4 +52,18 @@ void HousingObject::Update()
 
 void HousingObject::Render(HDC hdc)
 {
+	for (int y = 0; y < TileCountY; ++y)
+	{
+		for (int x = 0; x < TileCountX; ++x)
+		{
+			
+			mTileList[y][x]->Render(hdc);
+			
+		}
+	}
+}
+
+void HousingObject::Load()
+{
+
 }

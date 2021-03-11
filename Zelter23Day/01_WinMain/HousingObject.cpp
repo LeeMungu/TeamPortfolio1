21d304a3cc45e3bea6e4f7348c141e115879c2a4
whara123 <64121533+whara123@users.noreply.github.com>
2021-03-11 15:11:18 +0900

    하우징오브젝트
    일단1

--- Zelter23Day/01_WinMain/HousingObject.cpp
+++ Zelter23Day/01_WinMain/HousingObject.cpp
@@ -1,2 +1,22 @@
 #include "pch.h"
 #include "HousingObject.h"
+
+HousingObject::HousingObject(const string& name)
+{
+}
+
+void HousingObject::Init()
+{
+}
+
+void HousingObject::Release()
+{
+}
+
+void HousingObject::Update()
+{
+}
+
+void HousingObject::Render(HDC hdc)
+{
+}

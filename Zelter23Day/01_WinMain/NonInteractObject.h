51d9f72e31d6f31cdfec09379f76ff4332f30081
eshilia <corea_89@naver.com>
2021-03-11 14:59:55 +0900

    클래스 수정

--- Zelter23Day/01_WinMain/NonInteractObject.h
+++ Zelter23Day/01_WinMain/NonInteractObject.h
@@ -1,5 +1,16 @@
 #pragma once
-class NonInteractObject
+#include "GameObject.h"
+
+class Image;
+class Animation;
+class NonInteractObject : public GameObject
 {
+	Image* mImage;
+
+public:
+	NonInteractObject(const string name, float x, float y);
+	void Init()override;
+	void Release()override;
+	void Render(HDC hdc)override;
 };
 

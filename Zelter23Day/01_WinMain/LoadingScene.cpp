b9488b57404f29686e8d7c68f58f6b195d4eba1a
EasyVeryEasy <wlgns7010@naver.com>
2021-03-12 14:32:58 +0900

    ㅋㅋ
    ㅋㅋㅋㅋ

--- Zelter23Day/01_WinMain/LoadingScene.cpp
+++ Zelter23Day/01_WinMain/LoadingScene.cpp
@@ -14,6 +14,27 @@ void LoadingScene::Init()
 
 	//이미지 (플레이어)
 	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"PlayerRun", Resources(L"Player_run.png"),7,4); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"PlayerWalk", Resources(L"Player_walk.png"), 7, 4); });
+
+	//이미지 (몬스터)
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"MonsterRun", Resources(L"zombie01.png"), 5, 6); });
+
+	//이미지 (오브젝트)
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bench1", Resources(L"Bench1.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bench2", Resources(L"Bench2.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bench1", Resources(L"Bench1.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus1", Resources(L"Bus1.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus2", Resources(L"Bus2.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus3", Resources(L"Bus3.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus4", Resources(L"Bus4.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus5", Resources(L"Bus5.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus6", Resources(L"Bus6.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus7", Resources(L"Bus7.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus8", Resources(L"Bus8.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus9", Resources(L"Bus9.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus10", Resources(L"Bus10.png"), 2, 1); });
+	AddLoadFunc([]() {IMAGEMANAGER->LoadFromFile(L"Bus11", Resources(L"Bus11.png"), 2, 1); });
+
 
 
 
@@ -38,4 +59,5 @@ void LoadingScene::Update()
 
 void LoadingScene::Render(HDC hdc)
 {
+	mLoadingImage->Render(hdc, 0, 0);
 }

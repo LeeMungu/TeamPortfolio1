b6b2e700222f31951087d2b144f202c2e91a6e34
JongHOon <62693895+eshilia@users.noreply.github.com>
2021-03-17 12:27:10 +0900

    불필요한 타일 충돌 부분 제거

--- Zelter23Day/01_WinMain/scene1.cpp
+++ Zelter23Day/01_WinMain/scene1.cpp
@@ -160,33 +160,7 @@ void scene1::Update()
 				}
 				else if (mTileList[y][x]->GetTileLayer() == TileLayer::wall)
 				{
-					float tempW = temp.right - temp.left;
-					float tempH = temp.bottom - temp.top;
-					float tempX = temp.left + tempW / 2;
-					float tempY = temp.top + tempW / 2;
 
-					float tileRectX = tileRect.left + (tileRect.right - tileRect.left) / 2;
-					float tileRectY = tileRect.top + (tileRect.bottom - tileRect.top) / 2;
-
-					if (tempW < tempH && tempX > tileRectX && playerRect.left < tileRect.right)
-					{
-						tempPlayer->SetX(tileRect.right + tempPlayer->GetSizeX() / 2);
-					}
-
-					if (tempW < tempH && tempX < tileRectX && playerRect.right > tileRect.left)
-					{
-						tempPlayer->SetX(tileRect.left - tempPlayer->GetSizeX() / 2);
-					}
-
-					if (tempW > tempH && tempY > tileRectY && playerRect.top < tileRect.bottom)
-					{
-						tempPlayer->SetY(tileRect.bottom + tempPlayer->GetSizeY() / 2);
-					}
-
-					if (tempW > tempH && tempY < tileRectY && playerRect.bottom > tileRect.top)
-					{
-						tempPlayer->SetY(tileRect.top - tempPlayer->GetSizeY() / 2);
-					}
 				}
 			}
 		}

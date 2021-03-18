095ade379783c4106e7cf18b0146e140379fa6a5
choizyeon <chlwldjs99@gmail.com>
2021-03-18 17:34:07 +0900

    Revert "아이템 -틀-"
    -틀-"

--- Zelter23Day/01_WinMain/Item.cpp
+++ Zelter23Day/01_WinMain/Item.cpp
@@ -1,21 +1,9 @@
 #include "pch.h"
 #include "Item.h"
 #include "Image.h"
-#include "Camera.h"
-Item::Item(wstring imageKey, float x, float y, ItemType type) {
-	mKeyName = imageKey;
-	mX = x;
-	mY = y;
-	mType = type;
-}
-void Item::Init()
-{
-	//keyName에 따라 이미지 정해줌
-	if (mKeyName == L"Revolver") mImage = IMAGEMANAGER->FindImage(L"Weapon_pistol");
 
-	mSizeX = mImage->GetFrameWidth();
-	mSizeX = mImage->GetFrameHeight();
-	mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
+void Item::Init(float x, float y, ItemType type)
+{
 }
 
 void Item::Release()
@@ -24,11 +12,8 @@ void Item::Release()
 
 void Item::Update()
 {
-	//플레이어 충돌 후 인벤토리에 저장
 }
 
 void Item::Render(HDC hdc)
 {
-	CameraManager::GetInstance()->GetMainCamera()->
-		Render(hdc, mImage, mRect.left, mRect.top);
 }
--- Zelter23Day/01_WinMain/Item.h
+++ Zelter23Day/01_WinMain/Item.h
@@ -7,23 +7,26 @@ enum class ItemType
 	Equipment, // 장비
 	Weapon, // 무기
 	Food, // 음식
-	Drink, // 음료
 	Material // 재료
+
 };
 
-class Item : public GameObject
+class Item : GameObject
 {
 protected:
 	Image* mImage;
-	ItemType mType;
+	int mIndexX;
+	int mIndexY;
+	int mFrameCount;
 
-	wstring mKeyName;
 
 public:
-	Item(wstring imageKey, float x, float y, ItemType type);
-	void Init()override;
-	void Release()override;
-	void Update()override;
-	void Render(HDC hdc)override;
+	void Init(float x, float y, ItemType type);
+	void Release();
+	void Update();
+	void Render(HDC hdc);
+
+
+
 };
 

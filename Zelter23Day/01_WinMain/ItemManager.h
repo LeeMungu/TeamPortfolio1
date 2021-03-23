7ddec0b483e0d9c6979943b57262ce6ac860596e
choizyeon <43054183+choizyeon@users.noreply.github.com>
2021-03-24 00:44:25 +0900

    아이템 + 아이템 매니저 수정

--- Zelter23Day/01_WinMain/ItemManager.h
+++ Zelter23Day/01_WinMain/ItemManager.h
@@ -21,6 +21,7 @@ public:
 	void Update();
 	void Render(HDC hdc);
 
+	ItemType SetItemType(wstring key);
 	void randomItem(wstring objectKey, float x, float y);
 	void DropItems(wstring key, float x, float y);
 	void PickUpItems();
